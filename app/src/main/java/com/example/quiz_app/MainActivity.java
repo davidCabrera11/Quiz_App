dff855c9b2cef22e7689b7cb12af5ceee1836167
davidCabrera11 <davidcabrera052196@gmail.com>
2019-03-18 15:40:27 -0700

    Changed hint text color to black Added a text style for the user input edit text Added a button style for the quiz button

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.quiz_app;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        EditText yourName = (EditText) findViewById(R.id.yourName);
+        EditText friendName = (EditText) findViewById(R.id.friendName);
+
+        yourName.setHintTextColor(Color.GRAY);
+        friendName.setHintTextColor(Color.GRAY);
+
+
+
     }
 }
